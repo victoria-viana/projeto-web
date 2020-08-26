2d234addb0fbcbf24f4b5db1571d748c1378ee69
victoria-viana <victoriarviana@gmail.com>
2020-08-26 10:40:30 -0300

    Menus Finalizados

---
+++ frontend/js/menu.js
@@ -0,0 +1,11 @@
+
+document.querySelector('.menu-icon').addEventListener('click', (e) => {
+    e.stopPropagation();
+    document.querySelector('.profile').classList.remove('opened')
+    document.querySelector('.menu').classList.toggle('opened');
+})
+document.querySelector('.user-nav').addEventListener('click', (e) => {
+    e.stopPropagation();
+    document.querySelector('.menu').classList.remove('opened')
+    document.querySelector('.profile').classList.toggle('opened');
+})
