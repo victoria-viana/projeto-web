aa6b7a1ce692f84e93c5c6fb177fd4fcecceaec5
Everton Fr <evertonfreitas59@hotmail.com>
2020-09-03 12:57:31 -0300

    Vamos apresentar

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 3333;
 
 const app = express();
 
+const app1 = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
@@ -18,6 +20,8 @@ app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, 'uploads')));
 
+app1.use('/', express.static(path.resolve(__dirname, '..', '..', 'frontend')));
+
 try {
     mongoose.connect(`mongodb+srv://dbAdmin:${process.env.DBPASSWD}@cluster-rode.anech.mongodb.net/rodetransportesvistoria?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
@@ -25,6 +29,10 @@ try {
     });
 
     app.listen(port, () => {
+        console.log("Server started...");
+    });
+
+    app1.listen(5555, () => {
         console.log("Server started...");
     });
 } catch (error) {
